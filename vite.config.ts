028121960170604f9558dcdbf5421991a604d402
Krishna Rani Sahoo <krishnaa.sahoo@gmail.com>
2026-04-20 14:24:27 +0000

    Update vite.config.ts for adding test modules

--- vite.config.ts
+++ vite.config.ts
@@ -1,8 +1,15 @@
-import { defineConfig } from 'vite'
+import { defineConfig } from 'vitest/config'
 import RubyPlugin from 'vite-plugin-ruby'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
   plugins: [
     RubyPlugin(),
+    react(),
   ],
+  test: {
+    globals: true,
+    environment: 'jsdom',
+    setupFiles: './app/frontend/test/setup.ts',
+  },
 })
