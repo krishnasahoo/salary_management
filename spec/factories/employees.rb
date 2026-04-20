77e4e4f41d57cadb12e183e5dcceba6e49f8b3e3
Krishna Rani Sahoo <krishnaa.sahoo@gmail.com>
2026-04-20 15:51:20 +0000

    updated the employee factory bot

--- spec/factories/employees.rb
+++ spec/factories/employees.rb
@@ -1,13 +1,13 @@
 FactoryBot.define do
   factory :employee do
-    full_name { "MyString" }
-    job_title { "MyString" }
-    department { "MyString" }
-    country { "MyString" }
-    email { "MyString" }
-    salary { "9.99" }
-    employment_type { "MyString" }
-    hired_on { "2026-04-20" }
-    active { false }
+    full_name       { Faker::Name.name }
+    job_title       { Faker::Job.title }
+    department      { Faker::Commerce.department(max: 1) }
+    country         { Faker::Address.country }
+    email           { Faker::Internet.unique.email }
+    salary          { rand(30_000.0..200_000.0).round(2) }
+    employment_type { %w[full_time part_time contract].sample }
+    hired_on        { Faker::Date.between(from: 5.years.ago, to: Date.today) }
+    active          { true }
   end
 end
